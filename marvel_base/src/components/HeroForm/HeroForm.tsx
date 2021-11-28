84169a4ac5bb73deaa2e65b3f514ef13d2631a8a
nic0mp <nico.mp215@gmail.com>
2021-11-27 21:19:28 -0600

    + Signin folder

---
+++ marvel_base/src/components/HeroForm/HeroForm.tsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import {useDispatch, useSelector, useStore} from 'react-redux';
+import {useForm} from 'react-hook-form';
+import { chooseName, choosePrice} from '../../redux/slices/rootSlice';
+import {Input} from '../sharedComponents/Input';
+import {Button} from '@mui/material';
+
+import {server_calls} from '../../api';
+import {useGetData} from '../../custom-hooks';
+
+
+interface HeroFormProps {
+    id?: string;
+    data?: {}
+}
+
+interface HeroState {
+    name: string;
+    price: string;
+}
+
+export const DroneForm = (props:HeroFormProps) =>{
+    const dispatch = useDispatch()
+    const store  = useStore()
+    let {droneData, getData} = useGetData();
+    const name = useSelector<HeroState>(state => state.name)
+    const price = useSelector<HeroState>(state => state.price)
+    const { register, handleSubmit } = useForm({})
+    
+    const onSubmit = async (data:any, event:any) => {
+        console.log(props.id)
+
+        if(props.id!){
+            await server_calls.update(props.id!, data)
+            console.log(`Updated: ${data} ${props.id}`)
+            window.location.reload()
+            event.target.reset();
+        } else {
+            dispatch(chooseName(data.name))
+            dispatch(choosePrice(data.price))
+            await server_calls.create(store.getState())
+            window.location.reload()
+        }
+    }
+
+    return (
+        <div>
+            <form onSubmit = {handleSubmit(onSubmit)}>
+                <div>
+                    <label htmlFor="name">Drone Name</label>
+                    <Input {...register('name')} name="name" placeholder='Name' />
+                </div>
+                <div>
+                    <label htmlFor="price">Price</label>
+                    <Input {...register('price')} name="price" placeholder="Price"/>
+                </div>
+                <div>
+                    <label htmlFor="camera_quality">Camera Quality</label>
+                    <Input {...register('camera_quality')} name="camera_quality" placeholder="Camera Quality"/>
+                </div>
+                <div>
+                    <label htmlFor="flight_time">Flight Time</label>
+                    <Input {...register('flight_time')} name="flight_time" placeholder="Flight Time"/>
+                </div>
+                <div>
+                    <label htmlFor="description">Description</label>
+                    <Input {...register('description')} name="description" placeholder="Description"/>
+                </div>
+                <div>
+                    <label htmlFor="dimensions">Dimensions</label>
+                    <Input {...register('dimensions')} name="dimensions" placeholder="Dimensions"/>
+                </div>
+                <div>
+                    <label htmlFor="max_speed">Max Speed</label>
+                    <Input {...register('max_speed')} name="max_speed" placeholder="Max Speed"/>
+                </div>
+                <div>
+                    <label htmlFor="weight">Weight</label>
+                    <Input {...register('weight')} name="weight" placeholder="Weight"/>
+                </div>
+                <div>
+                    <label htmlFor="cost_of_production">Cost Of Production</label>
+                    <Input {...register('cost_of_production')} name="cost_of_production" placeholder="cost_of_production"/>
+                </div>
+                <div>
+                    <label htmlFor="series">Series</label>
+                    <Input {...register('series')} name="series" placeholder="Series"/>
+                </div>
+                <Button type='submit'>Submit</Button>
+            </form>
+        </div>
+    )
+}
